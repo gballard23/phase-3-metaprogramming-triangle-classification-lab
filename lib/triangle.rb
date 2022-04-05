a98cba2c60e0b634c11bc216135ac4269a665e20
Gerren <g.ballard23@outlook.com>
2022-04-05 15:58:35 -0500

    commit tree 7d157d3fa55ff83b8ec0782f92d39fe059d80680

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,42 @@
 class Triangle
-  # write code here
+  attr_accessor :length1, :length2, :length3
+
+  def initialize(length1, length2, length3)
+    @length1 = length1
+    @length2 = length2
+    @length3 = length3
+    
+  end
+
+  def kind
+    validate_triangle
+    if length1 == length2 && @length2 == length3
+    :equilateral
+    elsif length1 == length2 || length2 == length3 || length1 == length3
+    :isosceles
+    else
+    :scalene
+    end
+    
+
+  end
+
+  def valid_side?
+    [length1, length2, length3].all?(&:positive?)
+  end
+
+  def valid_triangle?
+    length1 + length2 > length3 && length2 + length3 > length1 && length3 + length1 > length2
+  end
+
+  def validate_triangle
+    raise TriangleError unless valid_side? && valid_triangle?
+  end
+
+
+
+
+  class TriangleError < StandardError
+  end
+
 end
